6b0da3fd1aa7e414b5e40c7eb2d2a01b5a12d507
Rohitpatil8951 <rohitpatil8951@gmail.com>
2024-06-06 14:30:49 +0530

    Added : Basics programs in c#

---
+++ Datatypes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    public class Datatypes
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            short s = 56;
+            Console.WriteLine(s);
+
+            int b = 14;
+            Console.WriteLine(b);
+            Console.WriteLine(int.MinValue);    
+            Console.WriteLine(int.MaxValue);
+
+            float y = 7.8f;
+            Console.WriteLine(y);
+
+            double d = 15;
+            Console.WriteLine(d); ;// by default its double
+
+            long l = 456789;
+            Console.WriteLine(l);
+
+            char a = 'G';
+            Console.WriteLine(a);
+
+            string ss = "Geeks";
+            Console.WriteLine(ss);
+
+            decimal money = 14.99m;
+            Console.WriteLine(money);
+            int x, y, z;
+
+
+        }
+    }
+}
